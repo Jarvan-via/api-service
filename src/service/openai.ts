7374dfe8e1ea4092ad4faf4c3d190843c39998da
袁勇 <you@example.com>
2023-03-19 21:37:00 +0800

    chore: update model

--- src/service/openai.ts
+++ src/service/openai.ts
@@ -33,7 +33,7 @@ export async function getChatCompletion(prompt: string, requestType: string): Pr
   const { apiKey, openai } = await getOpenAIClient(requestType);
   try {
     const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo-0301',
+      model: 'gpt-3.5-turbo',
       messages: [{
         role: 'user',
         content: prompt,
