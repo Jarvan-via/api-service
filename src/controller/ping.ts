5101de598d89f83914d3d5ec22092ab8091d8317
Jarvan <codeeeeeeeeeeeeeeeeeee@gmail.com>
2023-04-25 11:52:00 +0800

    feat: for status page

--- src/controller/ping.ts
+++ src/controller/ping.ts
@@ -1,39 +1,12 @@
 import express from 'express';
-import Joi from 'joi';
 
-import { resJSON, reqHandler, handler } from '../utils/helper';
+
+import { resJSON, reqHandler } from '../utils/helper';
 import Err from '../enums/code';
-import env from '../utils/env';
 
 const router = express.Router();
 
 router.all('/', reqHandler(async (req, res) => {
-  return res.json(resJSON(Err.CODE.SUCCESS, 'success', process.env, req.logCtx));
-}));
-
-const exampleSchema = Joi.object({
-  name: Joi.string().required(),
-  age: Joi.number().less(35),
-  skills: Joi.array().items(Joi.valid(...['java', 'nodejs', 'python'])),
-});
-
-interface ExampleValue {
-  name: string,
-  age: number,
-  skills: string[],
-}
-
-router.all('/example', reqHandler(async (req, res) => {
-  const { name, age, skills }: ExampleValue = await exampleSchema.validateAsync(req.body);
-
-  const config = await env._serv['cmd-list-manager'].post('/ping').then(handler);
-
-  return res.json(resJSON(Err.CODE.SUCCESS, 'success', { name, age, skills, config }, req.logCtx));
-}));
-
-router.all('/', reqHandler(async (req, res) => {
-  // 在这里写代码
-
   return res.json(resJSON(Err.CODE.SUCCESS, 'success', null, req.logCtx));
 }));
 
