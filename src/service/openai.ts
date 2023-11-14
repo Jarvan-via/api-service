4dfe90b7b706b30981233e0d024958895e89f223
Jarvan <codeeeeeeeeeeeeeeeeeee@gmail.com>
2023-11-14 13:34:00 +0800

    feat: update model

--- src/service/openai.ts
+++ src/service/openai.ts
@@ -33,7 +33,7 @@ export async function getChatCompletion(prompt: string, requestType: string): Pr
   const { apiKey, openai } = await getOpenAIClient(requestType);
   try {
     const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo-0613',
+      model: 'gpt-3.5-turbo-1106',
       messages: [{
         role: 'user',
         content: prompt,
